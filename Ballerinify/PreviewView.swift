517ac8935f7083eb50a8d6b2a28b63a9531bf01e
Kara Dietz <kmdietz@edu.uwaterloo.ca>
2020-05-06 15:34:01 -0400

    add camera files

---
+++ Ballerinify/PreviewView.swift
@@ -0,0 +1,35 @@
+//
+//  PreviewView.swift
+//  Ballerinify
+//
+//  Created by Kara Dietz on 2020-05-06.
+//  Copyright © 2020 Angela Dietz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+ /// The camera frame is displayed on this view.
+class PreviewView: UIView {
+    
+  var previewLayer: AVCaptureVideoPreviewLayer {
+    guard let layer = layer as? AVCaptureVideoPreviewLayer else {
+      fatalError("Layer expected is of type VideoPreviewLayer")
+    }
+    return layer
+  }
+
+  var session: AVCaptureSession? {
+    get {
+      return previewLayer.session
+    }
+    set {
+      previewLayer.session = newValue
+    }
+  }
+
+  override class var layerClass: AnyClass {
+    return AVCaptureVideoPreviewLayer.self
+  }
+}
